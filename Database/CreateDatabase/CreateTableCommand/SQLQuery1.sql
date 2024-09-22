15f2ac5eb847b31d95c22b624757ed8a76a860dc
mohamed ashrsf <malg12697@gmail.com>
2024-09-22 14:24:03 +0300

    Upload  Data base and installed all the necessary dependencies for Angular

---
+++ Database/CreateDatabase/CreateTableCommand/SQLQuery1.sql
@@ -0,0 +1,109 @@
+CREATE TABLE [User] (
+    UserID INT PRIMARY KEY IDENTITY(1,1),
+    Name NVARCHAR(50) NOT NULL,
+    Email NVARCHAR(50) NOT NULL UNIQUE,
+    Password NVARCHAR(50) NOT NULL,
+	Phone NVARCHAR(11) NOT NULL check(len(Phone)=11)
+	,
+    Role NVARCHAR(2) NOT NULL CHECK (Role IN ('T', 'A', 'S','t', 'a', 's' )), 
+    RegistrationDate DATE NOT NULL,
+    LastLoginDate DATE
+);
+
+CREATE TABLE Admin (
+    AdminId INT PRIMARY KEY 
+    FOREIGN KEY (AdminId) REFERENCES [User](UserID)ON UPDATE CASCADE ON DELETE CASCADE
+);
+CREATE TABLE Student (
+    StudentId INT PRIMARY KEY ,
+	 GradeLevel NVARCHAR(2) NOT NULL ,
+	 Governorate NVARCHAR(15) NOT NULL,
+	 ParentPhone NVARCHAR(11) NOT NULL check(len(ParentPhone)=11)
+    FOREIGN KEY (StudentId) REFERENCES [User](UserID) ON UPDATE CASCADE ON DELETE CASCADE
+);
+
+CREATE TABLE Teacher (
+    TeacherId INT PRIMARY KEY,
+   
+    FOREIGN KEY (TeacherId) REFERENCES [User](UserID)
+);
+
+CREATE TABLE Lesson (
+    LessonId INT PRIMARY KEY IDENTITY(1,1),
+    TeacherId INT NOT NULL,
+    GradeLevel NVARCHAR(2) NOT NULL,
+	 Description NVARCHAR(50) NOT NULL,
+    Title NVARCHAR(50) NOT NULL,
+    UploadDate DATE NOT NULL,
+    FeeAmount DECIMAL(10, 2) NOT NULL,
+    AccessPeriod int NOT NULL default(14),
+    FOREIGN KEY (TeacherId) REFERENCES Teacher(TeacherId)
+);
+CREATE TABLE Material (
+    MaterialId INT PRIMARY KEY IDENTITY(1,1),
+    LessonId INT NOT NULL,
+    MaterialType NVARCHAR(20) CHECK (MaterialType IN ('PDF', 'Video', 'Word', 'Reference')) NOT NULL,
+    MaterialLink NVARCHAR(max) NOT NULL,
+	Name NVARCHAR(50) NOT NULL
+    FOREIGN KEY (LessonId) REFERENCES Lesson(LessonId)
+	ON UPDATE CASCADE ON DELETE CASCADE
+);
+
+
+CREATE TABLE Receipt (
+    ReceiptId INT PRIMARY KEY IDENTITY(1,1),
+    ReceiptImageLink NVARCHAR(max) NOT NULL,
+    UploadDate DATE NOT NULL,
+	AdminReviewed  NVARCHAR(20) CHECK (AdminReviewed IN ('Y', 'N', 'y', 'n')) NOT NULL,
+);
+
+CREATE TABLE Enrollment (
+    EnrollmentId INT PRIMARY KEY IDENTITY(1,1),
+    LessonId INT NOT NULL, 
+    StudentId INT NOT NULL, 
+    ReceiptId INT NOT NULL, 
+    AccessStartDate DATE NOT NULL,
+	AccessEndDate DATE NOT NULL,
+    SubmissionDate DATE,
+    SubmissionLink NVARCHAR(max) default(''),
+    HomeWorkEvaluation NVARCHAR(25) default('Pending'),
+	UserName Varchar(25) not null unique,
+	Password Varchar(30) not null ,
+    ReceiptStatus NVARCHAR(20) NOT NULL,
+    FOREIGN KEY (LessonId) REFERENCES Lesson(LessonId)
+	ON UPDATE CASCADE 
+	,
+    FOREIGN KEY (StudentId) REFERENCES Student(StudentId)
+	ON UPDATE CASCADE ON DELETE CASCADE
+	,
+
+    FOREIGN KEY (ReceiptId) REFERENCES Receipt(ReceiptId)
+	ON UPDATE CASCADE ON DELETE CASCADE
+);
+
+
+
+CREATE TABLE Comment (
+    CommentID INT PRIMARY KEY IDENTITY(1,1),
+     Question TEXT NOT NULL,
+	 Reply TEXT NOT NULL,
+    QuestionDate   date ,
+	ReplyDate   date ,
+    
+);
+CREATE TABLE Student_Comment (
+    EnrollmentID INT PRIMARY KEY  IDENTITY(1,1),
+    CommentID INT not null,
+     LessonID INT not null,
+	StudentId INT NOT NULL, 
+	FOREIGN KEY (StudentId) REFERENCES Student(StudentId)
+	ON UPDATE CASCADE ON DELETE CASCADE
+	,
+    FOREIGN KEY (LessonID) REFERENCES Lesson(LessonID)
+	ON UPDATE CASCADE ON DELETE CASCADE
+	,
+	FOREIGN KEY (CommentID) REFERENCES Comment(CommentID)
+	ON UPDATE CASCADE ON DELETE CASCADE
+);
+
+
